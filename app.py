65d4a6bed64772231187d31ea50a321248db6b27
Kartik Srivastava <kartiksri1911@gmail.com>
2023-09-23 23:36:38 +0530

    Node and Flask Connected

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import numpy as np
 from flask import Flask , render_template , request , jsonify
+import json
 import pickle
 app = Flask(__name__)
 
 
 #Load the pickle model 
-model = pickle.load(open("C:\Codes\SIH\project\model.pkl" , "rb"))
+#model = pickle.load(open("C:\Codes\SIH\project\model.pkl" , "rb"))
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/predict", methods = ['POST'])
+def get_result():
+    data = request.get_json()
+    #print(data)
+    return json.dumps({"result": data['array']})
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require('express');
 const ejs=require('ejs');
+const request = require('request-promise');
 const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 let isBot=0;
+let result;
 const messages = [
     { id: 1, text: "Hi there!", sender: "bot" },
     { id: 2, text: "Hello!", sender: "user" },
@@ -19,31 +21,66 @@ app.get('/',(req,res)=>{
     const option={
         messages:messages
     }
+    //console.log(messages);
     res.render('home',option);
 });
-/* app.post('/',(req,res)=>{
+
+app.post('/chat',(req,res)=>{
     const text=req.body.text;
     const newid=messages[messages.length-1].id+1;
     messages.push({id:newid,text:text,sender:isBot?"bot":"user"});
     isBot=!isBot;
-    console.log(isBot);
+    console.log("CHat request done",isBot);
     res.redirect('/');
-}); */
-app.post('/', (req, res) => {
+});
+
+app.post('/', async (req, res) => {
     // Access the form data including checkbox values
+    // Checkbox values will be in checkboxData
     const checkboxData = req.body;
     const array=Object.values(checkboxData);
+    //delete first element with empty value
     array.shift();
-    // Checkbox values will be in checkboxData
-    setTimeout(() => {
+    const data={
+        array: array
+        
+    };
+
+    const options={
+        method: 'POST',
+        url: 'http://127.0.0.1:5000/predict',
+        body:data,
+        json: true
+    };
+
+   await request(options).then(function (parsedBody) {
+        //console.log(parsedBody);
+        result = parsedBody['result'];
+        console.log("results ", result);
+        const str=result.join(",");
+        const text="Got your disease! You are going to die in 2 seconds."+str;
+        const newid=messages[messages.length-1].id+1;
+        messages.push({id:newid,text: text,sender:isBot?"bot":"user"});
+        isBot=!isBot;
+        res.redirect('/');
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
+
+
+    //for syncronisation, had to use timeout
+    /* setTimeout(() => {
         console.log(array);
-    }, 20);
+    }, 20); */
   
     // Handle the form data, e.g., store it in a database, process it, etc.
     // ...
   
     // Redirect or render a response
-    res.redirect('/'); // Redirect to a thank-you page
+// Redirect back to home page
   });
 
 
